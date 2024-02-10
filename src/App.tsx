d9bfc171644e93ee7841d3e20a3ab74b4ac8a8d3
nardini-22 <arthur.nardini03@gmail.com>
2024-02-10 15:18:29 -0300

    chore: add and configure eslint, prettier and tailwindcss plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>teste</div>;
+  return (
+    <div className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+      teste
+    </div>
+  )
 }
 
-export default App;
+export default App
